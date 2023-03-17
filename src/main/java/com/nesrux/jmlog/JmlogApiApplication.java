cd2070e013a6a20754339cc20fd51789f0850501
Nesrux <joaomarcosdevs@gmail.com>
2023-03-17 14:58:25 -0300

    inicio do projeto

---
+++ src/main/java/com/nesrux/jmlog/JmlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.nesrux.jmlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nesrux/jmlog/JmlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nesrux.jmlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
