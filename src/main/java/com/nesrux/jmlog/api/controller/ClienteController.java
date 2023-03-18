9ee29cb6b95dd7b6ec9cd590361c07fb118c0689
Nesrux <joaomarcosdevs@gmail.com>
2023-03-18 17:24:10 -0300

    Criando o método Atualizar cliente com O verbo Put

--- src/main/java/com/nesrux/jmlog/api/controller/ClienteController.java
+++ src/main/java/com/nesrux/jmlog/api/controller/ClienteController.java
@@ -2,12 +2,12 @@ package com.nesrux.jmlog.api.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.nesrux.jmlog.domain.model.Cliente;
 import com.nesrux.jmlog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 @RequestMapping("/clientes")
 public class ClienteController {
 
-	@Autowired
 	private ClienteRepository clienteRepository;
 
 	@GetMapping()
@@ -32,9 +34,24 @@ public class ClienteController {
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 		return clienteRepository.findById(clienteId).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
 	}
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, 
+			@RequestBody Cliente cliente) {
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
 	}
 }
