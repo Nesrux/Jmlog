16adc518f034521c02a3467c1d6032ee1ecc812d
Nesrux <joaomarcosdevs@gmail.com>
2023-03-20 13:58:57 -0300

    Ja nem sei mais o oque colocar aqui

--- src/main/java/com/nesrux/jmlog/domain/service/SolicitacaoEntregaService.java
+++ src/main/java/com/nesrux/jmlog/domain/service/SolicitacaoEntregaService.java
@@ -1,5 +1,28 @@
 package com.nesrux.jmlog.domain.service;
 
+import java.time.LocalDateTime;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.nesrux.jmlog.domain.model.Entrega;
+import com.nesrux.jmlog.domain.model.StatusEntraga;
+import com.nesrux.jmlog.domain.repository.EntregaRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
 public class SolicitacaoEntregaService {
 
+	private EntregaRepository entregaRespository;
+
+	@Transactional
+	public Entrega solicitar(Entrega entrega) {
+		entrega.setStatus(StatusEntraga.PENDENTE);
+		entrega.setDataPedido(LocalDateTime.now());
+
+		return entregaRespository.save(entrega);
+	}
 }
