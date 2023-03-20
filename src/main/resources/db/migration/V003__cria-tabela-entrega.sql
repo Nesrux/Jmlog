ffe1ca1d76c5ae941f15c9d51561bb1cecb14a83
Nesrux <joaomarcosdevs@gmail.com>
2023-03-20 13:28:06 -0300

    Editando o arquivo V003 da migration

--- src/main/resources/db/migration/V003__cria-tabela-entrega.sql
+++ src/main/resources/db/migration/V003__cria-tabela-entrega.sql
@@ -1,11 +1,19 @@
-create table ocorrencia (
+create table entrega(
 	id bigint not null auto_increment,
-    entrega_id bigint not null,
-    descricao text not null,
-    data_registro datetime not null,
+    cliente_id bigint not null,
+    taxa decimal(10,2) not null,
+    status varchar(20) not null,
+    data_pedido datetime not null,
+    data_finalizacao datetime,
+    
+    destinatario_nome varchar(60) not null,
+    destinatario_logradouro varchar(255) not null,
+    destinatario_numero varchar(30) not null,
+    destinatario_complemento varchar(60) not null,
+    destinatario_bairro varchar(30) not null,
     
     primary key (id)
 );
 
-alter table ocorrencia add constraint fk_ocorrencia_entrega
-foreign key (entrega_id) references entrega (id);
+alter table entrega add constraint fk_entrega_cliente
+foreign key (cliente_id) references cliente (id);
